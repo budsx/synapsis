0fbcfeae747f44e8a16af091f55396d211c0636b
budi setiawan <budiskom99@gmail.com>
2025-09-12 15:32:07 +0700

    redis duplicate request

--- order-service/services/order_service.go
+++ order-service/services/order_service.go
@@ -2,12 +2,26 @@ package services
 
 import (
 	"context"
+	"errors"
+	"log/slog"
 
 	"github.com/budsx/synapsis/order-service/entity"
 	inventory "github.com/budsx/synapsis/order-service/repository/inventoryclient/proto"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *orderService) CreateOrder(ctx context.Context, req *entity.CreateOrderRequest) (*entity.CreateOrderResponse, error) {
+	allow, err := s.repo.Redis.DeduplicateCreateOrder(ctx, req.IdempotencyKey)
+	if err != nil {
+		return nil, err
+	}
+	if !allow {
+		return &entity.CreateOrderResponse{
+			Message: OrderStatusRejected.String(),
+		}, status.Error(codes.PermissionDenied, "duplicate request")
+	}
+
 	stock, err := s.repo.InventoryClient.CheckStock(ctx, &inventory.CheckStockRequest{
 		ProductId: req.ProductID,
 	})
@@ -21,13 +35,16 @@ func (s *orderService) CreateOrder(ctx context.Context, req *entity.CreateOrderR
 		}, nil
 	}
 
+	// Write order to database
+
 	err = s.repo.MessageQueue.PublishReserveStock(ctx, entity.ReserveStockRequest{
 		ProductID:      req.ProductID,
 		Quantity:       req.Quantity,
 		IdempotencyKey: req.IdempotencyKey,
 	})
 	if err != nil {
-		return nil, err
+		slog.ErrorContext(ctx, "failed to publish reserve stock", "error", err)
+		return nil, errors.New("failed to publish reserve stock")
 	}
 
 	return &entity.CreateOrderResponse{
